bcb5b18e085e58e27601e1cb755a805af03545c8
Mohit-code-592 <mr.mohitkumar592@gmail.com>
2026-02-19 08:10:39 +0530

    Update 05.py

--- 05.py
+++ 05.py
@@ -1,11 +1,14 @@
 # bubble sort
 def bubble_sort(arr, n):
     for i in range(n - 1):
+        isSorted = False
         for j in range(n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    return arr
-
+                isSorted = True
+        if isSorted == False:
+            break
+    
 arr = [2,4,1,3,5,10,7,9,8,6]
-result = bubble_sort(arr,len(arr))
-print(result)
+bubble_sort(arr, len(arr))
+print(arr)
